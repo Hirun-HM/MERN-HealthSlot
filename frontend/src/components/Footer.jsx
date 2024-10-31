ac3f111089ffa66e14da3004bc98ab2478333b5f
MSI <hirun08072002@gmail.com>
2024-10-31 10:55:07 +0530

    css adding to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets'
 
 const footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/*---left section-----*/}
             <div>
-                <img src= {assets.logo} alt='' />
-                <p>gfdhgfgjgdhgdddddjfjjjjjjjjjjjjj</p>
+                <img className='mb-5 w-40 ' src= {assets.logo} alt='' />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>gfdhgfgjgdhgdddddjfjjjjjjjjjjjjj</p>
 
             </div>
             {/*---center section-----*/}
             <div>
-                <p>Company</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>Company</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>about us</li>
                     <li>contact</li>
@@ -24,8 +24,8 @@ const footer = () => {
             </div>
             {/*---right section-----*/}
             <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+94-729827098</li>
                     <li>hirun080720002@gmail.com</li>
                 </ul>
