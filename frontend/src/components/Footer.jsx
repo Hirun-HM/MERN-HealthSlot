da902a6f65b566b43159b538c28ae219004effa9
jayathu sanka <shjayathusanka@gmail.com>
2024-11-03 15:15:18 +0530

    finishes more

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const footer = () => {
             {/*---left section-----*/}
             <div>
                 <img className='mb-5 w-40 ' src= {assets.logo} alt='' />
-                <p className='w-full md:w-2/3 text-gray-600 leading-6'>gfdhgfgjgdhgdddddjfjjjjjjjjjjjjj</p>
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem lpsm is simply dummy text of the printing and typesetting industry. Lorem lpsm has been the industry's standard dummy text ever since the 1500s.when an unknown printer took a gallery of type and scrambled it to make a type specimen book.</p>
 
             </div>
             {/*---center section-----*/}
