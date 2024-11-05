ceacd9e39531e48f100d8003d84d277c3b391693
MSI <hirun08072002@gmail.com>
2024-11-05 21:30:55 +0530

    add if

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -7,7 +7,10 @@
         
         //checking for all data to add doctor
         if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
-            
+            return res.json({
+                success: false,
+                message: "missing details"
+            })
         }
     }catch(error){
 
