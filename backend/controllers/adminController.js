9f82983d14c4ad7849295f9d2d5f81379f11107b
MSI <hirun08072002@gmail.com>
2024-11-06 08:52:48 +0530

    update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -98,6 +98,8 @@ const loginAdmin = async (req,res) =>{
     const {email,password} = req.body
     if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
       
+    }else {
+      res.json({success: false,message: "invalid credentials"})
     }
 
   }catch(error){
