f5ddddec6ae4cded3c1239e9e4e61fc8fdc6a2b0
MSI <hirun08072002@gmail.com>
2024-11-06 09:01:12 +0530

    changes

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -99,6 +99,10 @@ const loginAdmin = async (req,res) =>{
     const {email,password} = req.body
     if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
       const token= jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({
+        success: true,
+        token
+      })
     }else {
       res.json({success: false,message: "invalid credentials"})
     }
