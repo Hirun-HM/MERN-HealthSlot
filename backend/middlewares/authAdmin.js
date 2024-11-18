6fa8d8ef9ebeb777928fb24a21f10c3a80c31bcc
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 21:56:05 +0530

    changes

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -13,6 +13,7 @@ const authAdmin = async (req,res,next) => {
 
         }
         const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
+        console.log(token_decode);
 
         if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
             return res.json({
