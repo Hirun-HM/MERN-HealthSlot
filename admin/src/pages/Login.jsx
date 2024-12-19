b30dfbf274b8605d6e5cb85875697ecc045deefe
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-19 21:41:35 +0530

    login page

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,13 @@
-
+import { useState } from 'react'
+import {assets} from '../assets/assets'
 
 const Login = () => {
+
+const [state,setState] = useState('Admin')
+
   return (
     <div>
-      Login
+     
     </div>
   )
 }
