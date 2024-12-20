a68dc61a7b655ba319b9f83776d85cb5d37019be
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-20 09:19:10 +0530

    changes

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -9,12 +9,12 @@ const [state,setState] = useState('Admin')
   return (
     <form className='min-h-[80vh] flex items-center'>
     <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
-     <p><span>{state}</span>Login</p>
-     <div>
+     <p className='text-2xl font-semibold m-auto '><span className='text-primary'>{state}</span>Login</p>
+     <div className='w-full'>
      <p>Email</p>
      <input type='email' required />
      </div>
-     <div>
+     <div className='w-full'>
         <p>
             Password
         </p>
