4082285d0e61439fba320aab01e8a1347171bb1c
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-20 09:02:35 +0530

    styles

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -7,8 +7,8 @@ const Login = () => {
 const [state,setState] = useState('Admin')
 
   return (
-    <form>
-    <div>
+    <form className='min-h-[80vh] flex items-center'>
+    <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
      <p><span>{state}</span>Login</p>
      <div>
      <p>Email</p>
