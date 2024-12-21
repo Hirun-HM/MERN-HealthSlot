eeb23649d77492c6975acc2279c5f66371b001bb
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-21 09:39:44 +0530

    axios add

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -2,6 +2,7 @@
 import { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
@@ -10,8 +11,23 @@ const Login = () => {
 
   const { setAToken, backendUrl } = useContext(AdminContext);
 
+  const onSubmitHandler = async (event) => {
+event.preventDefault()
+
+try {
+    if (state === 'Admin') {
+        
+    }else{
+
+    }
+    
+} catch (error) {
+    
+}
+  }
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto ">
           <span className="text-primary">{state}</span>Login
