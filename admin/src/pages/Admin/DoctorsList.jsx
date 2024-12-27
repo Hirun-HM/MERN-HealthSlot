c549803c46f9e5429c9772ca8f61d4eb3feb76b8
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-27 15:59:39 +0530

    bug fix

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import  { useContext, useEffect } from "react"
 import { AdminContext } from "../../context/AdminContext"
 
 const DoctorsList = () => {
@@ -13,7 +13,7 @@ const DoctorsList = () => {
   },[aToken])
 
   return (
-    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">   
       <h1 className="text-lg font-medium">All Doctors</h1>
       <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
         {
