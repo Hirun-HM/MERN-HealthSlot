55244840e564f8284d4c2ad9a451122e08d88207
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-11 12:56:49 +0530

    change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -260,9 +260,13 @@ const payment = async (req, res) => {
       cancel_url: "http://localhost:5173/my-appointments",
     });
     console.log(session.id);
+
+    if (session.success) {
+      appointmentData.payment = true
+      await appointmentData.save()
+    }
     
-    appointmentData.payment = true
-    await appointmentData.save()
+   
 
     res.json({ success: true, id: session.id });
   } catch (error) {
