82ca46d568bfcfe9000ac182b34cc5c026bca015
jayathu sanka <shjayathusanka@gmail.com>
2025-02-01 07:55:39 +0530

    12.48.13

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -67,6 +67,20 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            
+            const {data} = await axios.post(backendUrl+'/api/admin/cancel-appointment',{appointmentId},{headers:{aToken}})
+            if(data.success){
+                toast.success(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
+
     const value = {
         aToken,setAToken,backendUrl,doctors,
         getAllDoctors,changeAvailability,
