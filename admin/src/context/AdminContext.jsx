cf101eab5a3694f77e550cd66853ccd88fd9ad1a
jayathu sanka <shjayathusanka@gmail.com>
2025-02-20 18:08:26 +0530

    13.00

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,6 +11,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState( localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors,setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -73,6 +74,9 @@ const AdminContextProvider = (props) => {
             const {data} = await axios.post(backendUrl+'/api/admin/cancel-appointment',{appointmentId},{headers:{aToken}})
             if(data.success){
                 toast.success(data.message)
+                getAllAppointments()
+            } else{
+                toast.error(data.message)
             }
 
         } catch (error) {
@@ -81,11 +85,27 @@ const AdminContextProvider = (props) => {
 
     }
 
+    const getDashData = async () =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard', {headers:{aToken}})
+
+            if(data.success){
+                setDashData(data.dashData)
+            } else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken,setAToken,backendUrl,doctors,
         getAllDoctors,changeAvailability,
         appointments,setAppointments,
-        getAllAppointments
+        getAllAppointments,
+        cancelAppointment,
+        dashData,getDashData
     }
 
     return (
