e4f1eb66e3f37121e7607dbd5df2fd2c593cdd0e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-02-24 20:30:44 +0530

    changes

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect } from 'react'
+import  { useContext,useEffect } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
 
 const DoctorAppointments = () => {
